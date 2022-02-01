d3b17aadda2aadedc6023a9e67099067eac74005
Chatelant <chatel.ant@gmail.com>
2022-02-01 11:34:01 +0100

    avancé

--- TP2/Renderer.h
+++ TP2/Renderer.h
@@ -13,128 +13,199 @@
 /// Namespace RayTracer
 namespace rt {
 
-  inline void progressBar( std::ostream& output,
-                           const double currentValue, const double maximumValue)
-  {
-    static const int PROGRESSBARWIDTH = 60;
-    static int myProgressBarRotation = 0;
-    static int myProgressBarCurrent = 0;
-    // how wide you want the progress meter to be
-    double fraction = currentValue /maximumValue;
-    
-    // part of the progressmeter that's already "full"
-    int dotz = static_cast<int>(floor(fraction * PROGRESSBARWIDTH));
-    if (dotz > PROGRESSBARWIDTH) dotz = PROGRESSBARWIDTH;
-    
-    // if the fullness hasn't changed skip display
-    if (dotz == myProgressBarCurrent) return;
-    myProgressBarCurrent = dotz;
-    myProgressBarRotation++;
-    
-    // create the "meter"
-    int ii=0;
-    output << "[";
-    // part  that's full already
-    for ( ; ii < dotz;ii++) output<< "#";
-    // remaining part (spaces)
-    for ( ; ii < PROGRESSBARWIDTH;ii++) output<< " ";
-    static const char* rotation_string = "|\\-/";
-    myProgressBarRotation %= 4;
-    output << "] " << rotation_string[myProgressBarRotation]
-           << " " << (int)(fraction*100)<<"/100\r";
-    output.flush();
-  }
-  
-  /// This structure takes care of rendering a scene.
-  struct Renderer {
+    inline void progressBar(std::ostream &output,
+                            const double currentValue, const double maximumValue) {
+        static const int PROGRESSBARWIDTH = 60;
+        static int myProgressBarRotation = 0;
+        static int myProgressBarCurrent = 0;
+        // how wide you want the progress meter to be
+        double fraction = currentValue / maximumValue;
 
-    /// The scene to render
-    Scene* ptrScene;
-    /// The origin of the camera in space.
-    Point3 myOrigin;
-    /// (myOrigin, myOrigin+myDirUL) forms a ray going through the upper-left
-    /// corner pixel of the viewport, i.e. pixel (0,0)
-    Vector3 myDirUL;
-    /// (myOrigin, myOrigin+myDirUR) forms a ray going through the upper-right
-    /// corner pixel of the viewport, i.e. pixel (width,0)
-    Vector3 myDirUR;
-    /// (myOrigin, myOrigin+myDirLL) forms a ray going through the lower-left
-    /// corner pixel of the viewport, i.e. pixel (0,height)
-    Vector3 myDirLL;
-    /// (myOrigin, myOrigin+myDirLR) forms a ray going through the lower-right
-    /// corner pixel of the viewport, i.e. pixel (width,height)
-    Vector3 myDirLR;
+        // part of the progressmeter that's already "full"
+        int dotz = static_cast<int>(floor(fraction * PROGRESSBARWIDTH));
+        if (dotz > PROGRESSBARWIDTH) dotz = PROGRESSBARWIDTH;
 
-    int myWidth;
-    int myHeight;
+        // if the fullness hasn't changed skip display
+        if (dotz == myProgressBarCurrent) return;
+        myProgressBarCurrent = dotz;
+        myProgressBarRotation++;
 
-    Renderer() : ptrScene( 0 ) {}
-    Renderer( Scene& scene ) : ptrScene( &scene ) {}
-    void setScene( rt::Scene& aScene ) { ptrScene = &aScene; }
-    
-    void setViewBox( Point3 origin, 
-                     Vector3 dirUL, Vector3 dirUR, Vector3 dirLL, Vector3 dirLR )
-    {
-      myOrigin = origin;
-      myDirUL = dirUL;
-      myDirUR = dirUR;
-      myDirLL = dirLL;
-      myDirLR = dirLR;
+        // create the "meter"
+        int ii = 0;
+        output << "[";
+        // part  that's full already
+        for (; ii < dotz; ii++) output << "#";
+        // remaining part (spaces)
+        for (; ii < PROGRESSBARWIDTH; ii++) output << " ";
+        static const char *rotation_string = "|\\-/";
+        myProgressBarRotation %= 4;
+        output << "] " << rotation_string[myProgressBarRotation]
+               << " " << (int) (fraction * 100) << "/100\r";
+        output.flush();
     }
 
-    void setResolution( int width, int height )
-    {
-      myWidth  = width;
-      myHeight = height;
-    }
+    /// This structure takes care of rendering a scene.
+    struct Renderer {
 
+        /// The scene to render
+        Scene *ptrScene;
+        /// The origin of the camera in space.
+        Point3 myOrigin;
+        /// (myOrigin, myOrigin+myDirUL) forms a ray going through the upper-left
+        /// corner pixel of the viewport, i.e. pixel (0,0)
+        Vector3 myDirUL;
+        /// (myOrigin, myOrigin+myDirUR) forms a ray going through the upper-right
+        /// corner pixel of the viewport, i.e. pixel (width,0)
+        Vector3 myDirUR;
+        /// (myOrigin, myOrigin+myDirLL) forms a ray going through the lower-left
+        /// corner pixel of the viewport, i.e. pixel (0,height)
+        Vector3 myDirLL;
+        /// (myOrigin, myOrigin+myDirLR) forms a ray going through the lower-right
+        /// corner pixel of the viewport, i.e. pixel (width,height)
+        Vector3 myDirLR;
 
-    /// The main rendering routine
-    void render( Image2D<Color>& image, int max_depth )
-    {
-      std::cout << "Rendering into image ... might take a while." << std::endl;
-      image = Image2D<Color>( myWidth, myHeight );
-      for ( int y = 0; y < myHeight; ++y ) 
-        {
-          Real    ty   = (Real) y / (Real)(myHeight-1);
-          progressBar( std::cout, ty, 1.0 );
-          Vector3 dirL = (1.0f - ty) * myDirUL + ty * myDirLL;
-          Vector3 dirR = (1.0f - ty) * myDirUR + ty * myDirLR;
-          dirL        /= dirL.norm();
-          dirR        /= dirR.norm();
-          for ( int x = 0; x < myWidth; ++x ) 
-            {
-              Real    tx   = (Real) x / (Real)(myWidth-1);
-              Vector3 dir  = (1.0f - tx) * dirL + tx * dirR;
-              Ray eye_ray  = Ray( myOrigin, dir, max_depth );
-              Color result = trace( eye_ray );
-              image.at( x, y ) = result.clamp();
+        int myWidth;
+        int myHeight;
+
+        struct Background {
+            virtual Color backgroundColor(const Ray &ray) = 0;
+        };
+
+        struct MyBackground : public Background {
+            Color backgroundColor(const Ray &ray) {
+                const double z = ray.direction[2];
+                if (z < 0) {
+                    Color result;
+                    Real x = -0.5f * ray.direction[0] / ray.direction[2];
+                    Real y = -0.5f * ray.direction[1] / ray.direction[2];
+                    Real d = sqrt(x * x + y * y);
+                    Real t = std::min(d, 30.0f) / 30.0f;
+                    x -= floor(x);
+                    y -= floor(y);
+                    if (((x >= 0.5f) && (y >= 0.5f)) || ((x < 0.5f) && (y < 0.5f)))
+                        result += (1.0f - t) * Color(0.2f, 0.2f, 0.2f) + t * Color(1.0f, 1.0f, 1.0f);
+                    else
+                        result += (1.0f - t) * Color(0.4f, 0.4f, 0.4f) + t * Color(1.0f, 1.0f, 1.0f);
+                    return result;
+                } else if (0 < z && z < 0.5) {
+                    return 0.5 * Color(1, 1, 1) + 0.5 * Color(0.0, 0.0, 1.0);
+                } else {
+                    return 0.5 * Color(0.0, 0.0, 1.0) + 0.5 * Color(0.0, 0.0, 0.0);
+                }
             }
+        };
+
+        /// The Background to render
+        Background *ptrBackground;
+
+        Renderer() : ptrScene(0), ptrBackground(0) {}
+
+        Renderer(Scene &scene) : ptrScene(&scene), ptrBackground(0) {}
+
+        void setScene(rt::Scene &aScene) { ptrScene = &aScene; }
+
+        void setViewBox(Point3 origin,
+                        Vector3 dirUL, Vector3 dirUR, Vector3 dirLL, Vector3 dirLR) {
+            myOrigin = origin;
+            myDirUL = dirUL;
+            myDirUR = dirUR;
+            myDirLL = dirLL;
+            myDirLR = dirLR;
         }
-      std::cout << "Done." << std::endl;
-    }
 
+        void setResolution(int width, int height) {
+            myWidth = width;
+            myHeight = height;
+        }
 
-    /// The rendering routine for one ray.
-    /// @return the color for the given ray.
-    Color trace( const Ray& ray )
-    {
-      assert( ptrScene != 0 );
-      Color result = Color( 0.0, 0.0, 0.0 );
-      GraphicalObject* obj_i = 0; // pointer to intersected object
-      Point3           p_i;       // point of intersection
+        /// The main rendering routine
+        void render(Image2D<Color> &image, int max_depth) {
+            std::cout << "Rendering into image ... might take a while." << std::endl;
+            image = Image2D<Color>(myWidth, myHeight);
+            for (int y = 0; y < myHeight; ++y) {
+                Real ty = (Real) y / (Real) (myHeight - 1);
+                progressBar(std::cout, ty, 1.0);
+                Vector3 dirL = (1.0f - ty) * myDirUL + ty * myDirLL;
+                Vector3 dirR = (1.0f - ty) * myDirUR + ty * myDirLR;
+                dirL /= dirL.norm();
+                dirR /= dirR.norm();
+                for (int x = 0; x < myWidth; ++x) {
+                    Real tx = (Real) x / (Real) (myWidth - 1);
+                    Vector3 dir = (1.0f - tx) * dirL + tx * dirR;
+                    Ray eye_ray = Ray(myOrigin, dir, max_depth);
+                    Color result = trace(eye_ray);
+                    image.at(x, y) = result.clamp();
+                }
+            }
+            std::cout << "Done." << std::endl;
+        }
 
-      // Look for intersection in this direction.
-      Real ri = ptrScene->rayIntersection( ray, obj_i, p_i );
-      // Nothing was intersected
-      if ( ri >= 0.0f ) return Color( 0.0, 0.0, 0.0 ); // some background color
+        // Affiche les sources de lumières avant d'appeler la fonction qui
+        // donne la couleur de fond.
+        Color background(const Ray &ray) {
+            Color result = Color(0.0, 0.0, 0.0);
+            for (Light *light: ptrScene->myLights) {
+                Real cos_a = light->direction(ray.origin).dot(ray.direction);
+                if (cos_a > 0.99f) {
+                    Real a = acos(cos_a) * 360.0 / M_PI / 8.0;
+                    a = std::max(1.0f - a, 0.0f);
+                    result += light->color(ray.origin) * a * a;
+                }
+            }
+            if (ptrBackground != 0) result += ptrBackground->backgroundColor(ray);
+            return result;
+        }
 
-      return obj_i->getMaterial(p_i).ambient + obj_i->getMaterial(p_i).diffuse;
+        /// The rendering routine for one ray.
+        /// @return the color for the given ray.
+        Color trace(const Ray &ray) {
+            assert(ptrScene != 0);
+            Color result = Color(0.0, 0.0, 0.0);
+            GraphicalObject *obj_i = 0; // pointer to intersected object
+            Point3 p_i;       // point of intersection
 
-      return Color( 1.0, 1.0, 1.0 );
-    }
+            // Look for intersection in this direction.
+            Real ri = ptrScene->rayIntersection(ray, obj_i, p_i);
+            // Nothing was intersected
+            if (ri >= 0.0f) return background(ray); // some background color
 
-  };
+            return illumination(ray, obj_i, p_i);
+            return Color(1.0, 1.0, 1.0);
+        }
+
+        /// Calcule le vecteur réfléchi à W selon la normale N.
+        Vector3 reflect(const Vector3 &W, Vector3 N) const {
+            const double tmp = W.dot(N);
+            const Vector3 res = W + Vector3((-2.0 * tmp) * N[0],
+                                            (-2.0 * tmp) * N[1],
+                                            (-2.0 * tmp) * N[2]);
+            if (W.dot(N) > 0) {
+                return Vector3(-1.0 * res[0], -1.0 * res[1], -1.0 * res[2]);
+            }
+            return res;
+        }
+
+        /// Calcule l'illumination de l'objet \a obj au point \a p, sachant que l'observateur est le rayon \a ray.
+        Color illumination(const Ray &ray, GraphicalObject *obj, Point3 p) {
+            Material m = obj->getMaterial(p);
+            Color c = Color();
+            for (int i = 0; i < (int) ptrScene->myLights.size(); i++) {
+                const Vector3 L = ptrScene->myLights[i]->direction(p);
+                const double Kd = L.dot(obj->getNormal(p));
+
+                // Specular
+                const Vector3 V = ray.direction;
+                const Vector3 W = reflect(V, obj->getNormal(p));
+                const double beta = W.dot(L);
+                if (beta > 0.0) {
+                    const double Ks = pow(beta, obj->getMaterial(p).shinyness);
+                    c = c + ptrScene->myLights[i]->color(p) * m.specular * Ks;
+                }
+                c = c + Kd * ptrScene->myLights[i]->color(p) * m.diffuse;
+            }
+            return c + m.ambient;
+        }
+    };
 
 } // namespace rt
 
